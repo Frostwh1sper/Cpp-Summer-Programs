e4c8b007e1d95fa872664c476e65fdc49e3c1640
Frostwh1sper <patrick.glenning@gmail.com>
2014-08-27 11:25:10 -0700

    Frostwh1sper

---
+++ Hello_World/src/hello_world.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Patrick Glenning"
+__date__ ="$Aug 27, 2014 11:23:57 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
